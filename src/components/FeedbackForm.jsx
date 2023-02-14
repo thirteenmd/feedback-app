f54e21d6bb52e44e9a20e1bb5cac688952e60f33
thirteenmd <andreacojocaru@gmail.com>
2023-02-14 13:04:22 +0200

    Added Feedback form and stats

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from './shared/Card'
+import { useState } from 'react'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input onChange={handleTextChange} value={text} type="text" placeholder='Write a review'/>
+          <button type='submit'>Send</button>
+        </div>
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
