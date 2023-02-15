4137f37cf9f4f24247102cb65f4c2b1a82b39e28
thirteenmd <andreacojocaru@gmail.com>
2023-02-15 16:01:20 +0200

    Changed file structure and fixed bug in db

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Header from "./components/Header"
+import Header from "./components/layout/Header"
+import AboutIconLink from './components/layout/AboutIconLink'
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
-import AboutIconLink from './components/AboutIconLink'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
