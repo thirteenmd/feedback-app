4280c2fb0e8b390fce5f71f349b654805f9d5e5c
thirteenmd <andreacojocaru@gmail.com>
2023-02-15 15:00:10 +0200

    Added json-server mock BE and integrated get feedback endpoint with FE

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,37 +1,28 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 8
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 9
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 7
-    },
-    {
-      id: 4,
-      text: 'This is feedback item 4',
-      rating: 10
-    }
-  ])
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async () => {
+    const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+    const data = await response.json()
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure you want to delete?')){
@@ -55,6 +46,7 @@ export const FeedbackProvider = ({children}) => {
   }
 
   return <FeedbackContext.Provider value={{
+    isLoading,
     feedback,
     feedbackEdit,
     deleteFeedback,
